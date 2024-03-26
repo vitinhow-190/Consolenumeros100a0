f6aef0c81ac99cab6ef444db820f03a7e8ce70cf
Usuario <Usuario@CPD02-M07>
2024-03-26 10:29:26 -0300

    Adicionar arquivos de projeto.

---
+++ Consolenumeros/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consolenumeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  
+            int cont = 100;
+
+            while (cont >= 0)
+            {
+                Console.WriteLine(cont);
+
+                cont--;// adiciona mais 1 automaticamente
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
